f42460dc928fb3524f46bfbebcb7e0da2e935f32
Joshua Acuros Kim <acuros@kaist.ac.kr>
2016-06-08 10:42:42 +0900

    App init

---
+++ app/webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        app: './app.jsx'
+    },
+    output: {
+        path: __dirname + '/../dist',
+        filename: '[name].js'
+    },
+    resolve: {
+        root: __dirname,
+        extensions: ['', '.js', '.jsx', '.less']
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx$/, loader: 'jsx' },
+            { test: /\.css$/, loader: 'style!css'},
+            { test: /\.less$/, loader: 'style!css!less'}
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
+    ],
+    externals: {
+        'react': 'React',
+        'jquery': '$',
+    }
+};
